f58c8278b1102f4237be4db9a47d572eec6f370b
Natan <natan-moreira1@hotmail.com>
2023-11-27 11:03:57 -0300

    add data layer

---
+++ Digix.Data/DataContext.cs
@@ -0,0 +1,37 @@
+﻿using Digix.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Digix.Data
+{
+    public class DataContext : DbContext 
+    {
+
+        public DataContext(DbContextOptions<DataContext> dbContext) : base(dbContext) {
+
+            AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        }
+
+        public DbSet<Person> People { get; set; }
+
+        public DbSet<Family> Families { get; set; }
+
+        public DbSet<FamilyAnalysis> FamilyAnalyses { get; set; }
+
+        public DbSet<AnalysisRules> AnalysisRules { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>()
+                .HasKey(p => p.Cpf);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
+/*
+ * dotnet ef migrations add Initial --context DataContext --startup-project .\Digix.Application.csproj --project ..\Digix.Data\ --output-dir ..\Digix.Data\Migrations
+ * dotnet ef database update --project ..\Digix.Data\ --startup-project .\Digix.Application.csproj
+ * dotnet ef migrations remove --project ..\Digix.Data --startup-project .\Digix.Application.csproj
+*/
---
+++ Digix.Data/Migrations/20231121214427_AddAnalysis.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Digix.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAnalysis : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FamilyAnalyses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Score = table.Column<int>(type: "integer", nullable: false),
+                    FamilyId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FamilyAnalyses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FamilyAnalyses_Families_FamilyId",
+                        column: x => x.FamilyId,
+                        principalTable: "Families",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AnalysisRules",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FamilyAnalysisId = table.Column<int>(type: "integer", nullable: false),
+                    FirstRule = table.Column<bool>(type: "boolean", nullable: false),
+                    SecondRule = table.Column<bool>(type: "boolean", nullable: false),
+                    ThirdRule = table.Column<bool>(type: "boolean", nullable: false),
+                    FourthRule = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnalysisRules", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AnalysisRules_FamilyAnalyses_FamilyAnalysisId",
+                        column: x => x.FamilyAnalysisId,
+                        principalTable: "FamilyAnalyses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AnalysisRules_FamilyAnalysisId",
+                table: "AnalysisRules",
+                column: "FamilyAnalysisId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FamilyAnalyses_FamilyId",
+                table: "FamilyAnalyses",
+                column: "FamilyId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnalysisRules");
+
+            migrationBuilder.DropTable(
+                name: "FamilyAnalyses");
+        }
+    }
+}
